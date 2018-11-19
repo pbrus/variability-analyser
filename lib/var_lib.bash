55f77f4c2072e4aba6a3deb9097f142100362ebd
Przemysław Bruś <brus@astro.uni.wroc.pl>
2018-11-19 10:48:44 +0100

    lib/var_lib.bash: add a set of functions to detrend the input lightcurve

--- lib/var_lib.bash
+++ lib/var_lib.bash
@@ -10,3 +10,44 @@ function rearrange_columns_lightcurve()
         print $'${LC_TIME_COLUMN}', $'${LC_MAG_COLUMN}', $'${LC_ERR_COLUMN}'
     }' $1 > $1.tmp
 }
+
+function display_detrend_data()
+{
+    detrend.py $1 ${1/${LC_DIR}/${LC_DETREND_DIR}} $2 --display
+}
+
+function save_detrend_data()
+{
+    detrend.py $1 ${1/${LC_DIR}/${LC_DETREND_DIR}} $2 --image
+}
+
+function detrend_data()
+{
+    local nodes_number=9
+    local answer="n"
+
+    echo "Is OK? [y/n]:"
+    display_detrend_data $1 ${nodes_number}
+    read answer
+
+    while [ "$answer" == "n" ]
+    do
+        echo "Number of nodes (last ${nodes_number}):"
+        read nodes_number
+        echo "Is OK? [y/n]:"
+        display_detrend_data $1 ${nodes_number}
+        read answer
+    done
+
+    save_detrend_data $1 ${nodes_number}
+}
+
+function detrend()
+{
+    if [ ! -e ${1/${LC_DIR}/${LC_DETREND_DIR}} ]
+    then
+        rearrange_columns_lightcurve $1
+        detrend_data $1
+        remove_files_or_dirs $1.tmp
+    fi
+}
