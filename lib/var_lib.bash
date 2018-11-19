8122e16ec30d2ccf6a143a23fe29a7fbc4095bdb
Przemysław Bruś <brus@astro.uni.wroc.pl>
2018-11-19 10:47:30 +0100

    lib/var_lib.bash: add function to rearrange columns order in the file with a lightcurve

--- lib/var_lib.bash
+++ lib/var_lib.bash
@@ -3,3 +3,10 @@ function welcome_message()
     print_welcome
     exit 1
 }
+
+function rearrange_columns_lightcurve()
+{
+    awk '{
+        print $'${LC_TIME_COLUMN}', $'${LC_MAG_COLUMN}', $'${LC_ERR_COLUMN}'
+    }' $1 > $1.tmp
+}
