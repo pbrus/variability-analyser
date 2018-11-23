bb45fc6de4fe77005364f32a574a9e030a20498c
Przemysław Bruś <brus@astro.uni.wroc.pl>
2018-11-23 13:24:52 +0100

    Move a few functions from lib/var_lib.bash to a new file lib/prepare_lc_lib.bash; add trim function to the lib/prepare_lc_lib.bash file

---
+++ lib/prepare_lc_lib.bash
@@ -0,0 +1,60 @@
+function rearrange_columns_lightcurve()
+{
+    awk '{
+        print $'${LC_TIME_COLUMN}', $'${LC_MAG_COLUMN}', $'${LC_ERR_COLUMN}'
+    }' $1 > $1.tmp
+}
+
+function display_detrend_data()
+{
+    local output_file=${1/${LC_DIR}/${LC_DETREND_DIR}}
+
+    detrend.py $1 ${output_file%".tmp"} $2 --display
+}
+
+function save_detrend_data()
+{
+    local output_file=${1/${LC_DIR}/${LC_DETREND_DIR}}
+
+    detrend.py $1 ${output_file%".tmp"} $2 --image
+}
+
+function detrend_data()
+{
+    local nodes_number=9
+    local answer="n"
+
+    echo "Is OK? [y/n]:"
+    display_detrend_data $1 ${nodes_number}
+    read answer
+
+    while [ "$answer" == "n" ]
+    do
+        echo "Number of nodes (last ${nodes_number}):"
+        read nodes_number
+        echo "Is OK? [y/n]:"
+        display_detrend_data $1 ${nodes_number}
+        read answer
+    done
+
+    save_detrend_data $1 ${nodes_number}
+}
+
+function detrend()
+{
+    if [ ! -e ${1/${LC_DIR}/${LC_DETREND_DIR}} ]
+    then
+        rearrange_columns_lightcurve $1
+        detrend_data $1.tmp
+        remove_files_or_dirs $1.tmp
+    fi
+}
+
+function trim()
+{
+    if [ ! -e ${1/${LC_DIR}/${LC_TRIM_DIR}} ]
+    then
+        trim.py ${1/${LC_DIR}/${LC_DETREND_DIR}} \
+        ${1/${LC_DIR}/${LC_TRIM_DIR}} --display --image
+    fi
+}
