171806ab035941dfd07b2483e557543355d0d351
Przemysław Bruś <brus@astro.uni.wroc.pl>
2018-12-18 23:47:40 +0100

    Take into account a time prefix of the Julian Date

--- lib/prepare_lc_lib.bash
+++ lib/prepare_lc_lib.bash
@@ -1,7 +1,8 @@
 function rearrange_columns_lightcurve()
 {
     awk '{
-        print $'${LC_TIME_COLUMN}', $'${LC_MAG_COLUMN}', $'${LC_ERR_COLUMN}'
+        printf("%f %f %f\n", $'${LC_TIME_COLUMN}'-'${LC_TIME_PREFIX}', \
+        $'${LC_MAG_COLUMN}', $'${LC_ERR_COLUMN}')
     }' $1 > $1.tmp
 }
 
