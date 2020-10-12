96c6b08dfb987a927bdf267f62f0902abe975515
Przemysław Bruś <19304263+pbrus@users.noreply.github.com>
2020-10-12 22:16:36 +0200

    Refactor phase.py: add type hints, fix docstrings, black format, optimize imports

--- varana/varana/phase.py
+++ varana/varana/phase.py
@@ -2,18 +2,20 @@
 Phase a light curve with a specific frequency.
 
 """
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
 from copy import deepcopy
-from warnings import filterwarnings
 from os.path import basename, splitext, dirname, join
+from typing import Tuple, Callable
+from warnings import filterwarnings
 
-filterwarnings("ignore",
-               message="Covariance of the parameters could not be estimated")
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import ndarray
+from scipy.optimize import curve_fit
+
+filterwarnings("ignore", message="Covariance of the parameters could not be estimated")
 
 
-def read_lightcurve(filename):
+def read_lightcurve(filename: str) -> ndarray:
     """
     Read a light curve from a file.
 
@@ -24,13 +26,14 @@ def read_lightcurve(filename):
 
     Returns
     -------
-    tuple
-        Each element represents a single column.
+    ndarray
+        Unpacked array which stores data from input file.
+
     """
     return np.loadtxt(filename, unpack=True)
 
 
-def read_model(filename):
+def read_model(filename: str) -> Tuple[float, ndarray]:
     """
     Read a model from a file. The model is defined as:
 
@@ -51,16 +54,16 @@ def read_model(filename):
     Returns
     -------
     tuple
-        The first element is a y intercept of the model, the second one is an
-        ndarray which stores parameters of each sine.
+        The first element is y intercept of the model, the second one is an array which stores parameters of each sine.
+
     """
     y_intercept = np.genfromtxt(filename, max_rows=1)
     parameters = np.genfromtxt(filename, skip_header=1)
 
-    return y_intercept, parameters
+    return float(y_intercept), parameters
 
 
-def time_to_phase(time, period):
+def time_to_phase(time: ndarray, period: float) -> ndarray:
     """
     Convert time to phase.
 
@@ -73,17 +76,18 @@ def time_to_phase(time, period):
 
     Returns
     -------
-    phase
+    phase : ndarray
         A phase vector.
+
     """
     time_min = time.min()
     time = time - time_min
-    phase = time/period - time//period
+    phase = time / period - time // period
 
     return phase
 
 
-def multiply_phase(phase, magnitude, factor):
+def multiply_phase(phase: ndarray, magnitude: ndarray, factor: int) -> Tuple[ndarray, ndarray]:
     """
     Multiply a phase vector by the factor.
 
@@ -100,6 +104,7 @@ def multiply_phase(phase, magnitude, factor):
     -------
     tuple
         A tuple made of phase and magnitude vectors.
+
     """
     single_phase = deepcopy(phase)
     single_magnitude = deepcopy(magnitude)
@@ -111,7 +116,7 @@ def multiply_phase(phase, magnitude, factor):
     return phase, magnitude
 
 
-def prepare_data(filename, frequency, phases_number):
+def prepare_data(filename: str, frequency: float, phases_number: int) -> Tuple[ndarray, ndarray]:
     """
     Phase a light curve with the frequency.
 
@@ -120,7 +125,7 @@ def prepare_data(filename, frequency, phases_number):
     filename : str
         A name of a file which stores a light curve.
     frequency : float
-        A value of the frequeny which phases the light curve with.
+        A value of the frequency which phases the light curve with.
     phases_number : int
         A number indicating how many phases to display.
 
@@ -128,15 +133,16 @@ def prepare_data(filename, frequency, phases_number):
     -------
     tuple
         A tuple made of phase and magnitude vectors.
+
     """
     time, magnitude, _ = read_lightcurve(filename)
-    phase = time_to_phase(time, 1/frequency)
+    phase = time_to_phase(time, 1 / frequency)
     phase, magnitude = multiply_phase(phase, magnitude, phases_number)
 
     return phase, magnitude
 
 
-def get_model(filename, frequency, phase, magnitude):
+def get_model(filename: str, frequency: float, phase: ndarray, magnitude: ndarray) -> Tuple[ndarray, ndarray]:
     """
     Get a model in XY coordinates and align it to the phased light curve.
 
@@ -145,7 +151,7 @@ def get_model(filename, frequency, phase, magnitude):
     filename : str
         A name of a file which stores a model.
     frequency : float
-        A value of the frequeny which phases the light curve with.
+        A value of the frequency which phases the light curve with.
     phase : ndarray
         An array which stores a phase vector.
     magnitude : ndarray
@@ -154,19 +160,20 @@ def get_model(filename, frequency, phase, magnitude):
     Returns
     -------
     tuple
-        A tuple made of X, Y coordinates.
+        A tuple made of (x, y) coordinates.
+
     """
     y_intercept, parameters = read_model(filename)
     min_phase, max_phase = int(round(phase.min())), int(round(phase.max()))
     approx_sines_sum = sines_sum(parameters, y_intercept, frequency)
     x0, _ = curve_fit(approx_sines_sum, phase, magnitude, p0=0)
-    X = np.linspace(min_phase, max_phase, max_phase*100)
+    x = np.linspace(min_phase, max_phase, max_phase * 100)
     model = sines_sum(parameters, y_intercept, frequency)
 
-    return X, model(X, x0)
+    return x, model(x, x0)
 
 
-def sines_sum(sines_parameters, y_intercept, frequency):
+def sines_sum(sines_parameters: ndarray, y_intercept: float, frequency: float) -> Callable:
     """
     Define a sum of sines.
 
@@ -177,28 +184,29 @@ def sines_sum(sines_parameters, y_intercept, frequency):
     y_intercept : float
         The place where the function is hooked on the y-axis.
     frequency : float
-        A value of the frequeny which phases the light curve with.
+        A value of the frequency which phases the light curve with.
 
     Returns
     -------
     function
-        The sum of sines with two arguments: x, x0.
-        x0 is a shift on the x-axis, i.e. f(x-x0).
+        The sum of sines with two arguments: x, x0. x0 is a shift on the x-axis, i.e. f(x-x0).
+
     """
-    def sines(x, x0):
+
+    def _sines(x, x0):
         y = 0
 
         for par in sines_parameters.reshape(-1, 3):
-            y += par[0]*np.sin(2*np.pi*par[1]/frequency*(x - x0) + par[2])
+            y += par[0] * np.sin(2 * np.pi * par[1] / frequency * (x - x0) + par[2])
 
         return y + y_intercept
 
-    return sines
+    return _sines
 
 
-def split_filename(filename):
+def split_filename(filename: str) -> Tuple[str, str]:
     """
-    Split a filename into a name and en extenstion.
+    Split a filename into a name and an extension.
 
     Parameters
     ----------
@@ -209,22 +217,23 @@ def split_filename(filename):
     -------
     tuple
         A tuple: (basename, extension).
+
     """
     return splitext(basename(filename))
 
 
-def _draw_phase(phase, magnitude, markersize=4):
+def _draw_phase(phase: ndarray, magnitude: ndarray, marker_size: int = 4) -> None:
     plt.xlabel("Phase")
     plt.ylabel("Brightness [mag]")
     plt.gca().invert_yaxis()
-    plt.plot(phase, magnitude, '.', alpha=0.5, markersize=markersize)
+    plt.plot(phase, magnitude, ".", alpha=0.5, markersize=marker_size)
 
 
-def _draw_model(X, Y):
-    plt.plot(X, Y, 'r-', alpha=0.7)
+def _draw_model(x: ndarray, y: ndarray) -> None:
+    plt.plot(x, y, "r-", alpha=0.7)
 
 
-def display_plot(phase, magnitude, model=None):
+def display_plot(phase: ndarray, magnitude: ndarray, model: Tuple[ndarray, ndarray] = None) -> None:
     """
     Display a phased light curve.
 
@@ -235,7 +244,8 @@ def display_plot(phase, magnitude, model=None):
     magnitude : ndarray
         An array which represents a magnitude vector.
     model : tuple
-        X, Y coordinates of the model.
+        (x, y) coordinates of the model.
+
     """
     _draw_phase(phase, magnitude, 6)
 
@@ -245,7 +255,7 @@ def display_plot(phase, magnitude, model=None):
     plt.show()
 
 
-def save_plot(phase, magnitude, filename, model=None):
+def save_plot(phase: ndarray, magnitude: ndarray, filename: str, model: Tuple[ndarray, ndarray] = None) -> None:
     """
     Save an image with a phased light curve to the file.
 
@@ -258,7 +268,8 @@ def save_plot(phase, magnitude, filename, model=None):
     filename : str
         The name of a file which will store the image of a phased light curve.
     model : tuple
-        X, Y coordinates of the model.
+        (x, y) coordinates of the model.
+
     """
     figure = plt.figure(figsize=(10, 5), dpi=150)
     figure.add_subplot(111)
@@ -267,12 +278,11 @@ def save_plot(phase, magnitude, filename, model=None):
     if model is not None:
         _draw_model(*model)
 
-    png_filename = join(dirname(filename), split_filename(filename)[0]
-                        + ".png")
+    png_filename = join(dirname(filename), split_filename(filename)[0] + ".png")
     figure.savefig(png_filename)
 
 
-def save_phased_lightcurve(phase, magnitude, filename):
+def save_phased_lightcurve(phase: ndarray, magnitude: ndarray, filename: str) -> None:
     """
     Save a phased light curve to the file.
 
@@ -284,6 +294,7 @@ def save_phased_lightcurve(phase, magnitude, filename):
         An array which represents a magnitude vector.
     filename : str
         The name of a file which will store a phased light curve.
+
     """
     result = np.append(phase.reshape(1, -1), magnitude.reshape(1, -1), axis=0)
     np.savetxt(filename, result.T, fmt="%16.6f %9.4f")
