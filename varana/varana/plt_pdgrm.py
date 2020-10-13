0be1fa8beeaa4714aec9622c9a3ecfea4615d459
Przemysław Bruś <19304263+pbrus@users.noreply.github.com>
2020-10-13 20:05:39 +0200

    Refactor plt_pdgrm.py: add type hints, black format, optimize imports

--- varana/varana/plt_pdgrm.py
+++ varana/varana/plt_pdgrm.py
@@ -2,12 +2,13 @@
 Display or save to a file a periodogram.
 
 """
-import matplotlib.pyplot as plt
-from numpy import genfromtxt
 from re import search
 
+import matplotlib.pyplot as plt
+from numpy import genfromtxt, ndarray
 
-def get_data(filename):
+
+def get_data(filename: str) -> ndarray:
     """
     Get data from a file with two columns.
 
@@ -18,22 +19,20 @@ def get_data(filename):
 
     Returns
     -------
-    tuple
-        A tuple with two ndarrays. Each element stores one column of
-        the input file.
+    ndarray
+        Unpacked array which stores data from input file.
+
     """
-    x, y = genfromtxt(filename, unpack=True)
-
-    return x, y
+    return genfromtxt(filename, unpack=True)
 
 
-def _draw_periodogram(frequency, amplitude):
+def _draw_periodogram(frequency: ndarray, amplitude: ndarray) -> None:
     plt.xlabel("Frequency [c/d]")
     plt.ylabel("Amplitude [mag]")
-    plt.plot(frequency, amplitude, '-')
+    plt.plot(frequency, amplitude, "-")
 
 
-def display_periodogram(frequency, amplitude):
+def display_periodogram(frequency: ndarray, amplitude: ndarray) -> None:
     """
     Display a periodogram.
 
@@ -43,12 +42,13 @@ def display_periodogram(frequency, amplitude):
         An array which represents frequencies.
     amplitude : ndarray
         An array which stores values of amplitude for each frequency.
+
     """
     _draw_periodogram(frequency, amplitude)
     plt.show()
 
 
-def save_periodogram(frequency, amplitude, filename):
+def save_periodogram(frequency: ndarray, amplitude: ndarray, filename: str) -> None:
     """
     Save a periodogram to the file.
 
@@ -60,12 +60,13 @@ def save_periodogram(frequency, amplitude, filename):
         An array which stores values of amplitude for each frequency.
     filename : str
         The name of a PNG file to which to save a plot.
+
     """
     figure = plt.figure(figsize=(10, 5), dpi=150)
     figure.add_subplot(111)
     _draw_periodogram(frequency, amplitude)
 
-    if search(r'[.]png$', filename):
+    if search(r"[.]png$", filename):
         png_filename = filename
     else:
         png_filename = filename + ".png"
