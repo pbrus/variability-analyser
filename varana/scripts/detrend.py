33a2603897101155799a154cd05b1eee9b1f376d
Przemysław Bruś <19304263+pbrus@users.noreply.github.com>
2020-10-13 21:21:49 +0200

    Refactor detrend.py script

--- varana/scripts/detrend.py
+++ varana/scripts/detrend.py
@@ -1,66 +1,99 @@
 #!/usr/bin/env python3
+"""
+Detrend a light curve removing seasonal deviations.
 
+"""
 from argparse import ArgumentParser, RawTextHelpFormatter, ArgumentTypeError
+from copy import deepcopy
 from textwrap import dedent
-from varana.detrend import *
 
+from numpy import savetxt
 
-argparser = ArgumentParser(
-    prog='detrend.py',
-    description='>> Removes seasonal trends from a light curve <<',
-    epilog='Copyright (c) 2019 Przemysław Bruś',
-    formatter_class=RawTextHelpFormatter
+from varana.detrend import (
+    calculate_kmeans,
+    detrend_data,
+    display_plot,
+    get_data,
+    save_plot,
+    sigma_clipping_magnitude,
+    sorted_centers,
+    spline_function,
+    spline_order,
+    too_much_points_rejected,
+    unpack_data,
+    valid_seasons_amount,
+    warn_rejected_points,
+    x_domain_spline,
 )
 
-argparser.add_argument(
-    'input_lightcurve',
-    help=dedent('''\
+arg_parser = ArgumentParser(
+    prog="detrend.py",
+    description=">> Remove seasonal trends from a light curve <<",
+    epilog="Copyright (c) 2020 Przemysław Bruś",
+    formatter_class=RawTextHelpFormatter,
+)
+
+arg_parser.add_argument(
+    "input_lightcurve",
+    help=dedent(
+        """\
     The name of a file which stores light curve data.
     ------------------------------------
     The file must contain three columns:
     time magnitude magnitude_error
 
-    ''')
+    """
+    ),
 )
 
-argparser.add_argument(
-    'output_lightcurve',
-    help=dedent('''\
+arg_parser.add_argument(
+    "output_lightcurve",
+    help=dedent(
+        """\
     The name of a file which will store a detrended light curve.
 
-    ''')
+    """
+    ),
 )
 
-argparser.add_argument(
-    'seasons_amount',
-    help=dedent('''\
+arg_parser.add_argument(
+    "seasons_amount",
+    help=dedent(
+        """\
     The number of seasons in the data.
     The seasons are separated by time-gaps in the data.
 
-    '''),
-    type=int
+    """
+    ),
+    type=int,
 )
 
-argparser.add_argument(
-    '--display',
-    help=dedent('''\
+arg_parser.add_argument(
+    "--display",
+    help=dedent(
+        """\
     Display a plot.
 
-    '''),
-    action='store_true'
+    """
+    ),
+    action="store_true",
 )
 
-argparser.add_argument(
-    '--image',
-    help=dedent('''\
+arg_parser.add_argument(
+    "--image",
+    help=dedent(
+        """\
     Save a plot to the PNG file.
     The name of the image will be the same as for output file.
 
-    '''),
-    action='store_true'
+    """
+    ),
+    action="store_true",
 )
 
-args = argparser.parse_args()
+args = arg_parser.parse_args()
+data, seasons_amount = None, None
+
 try:
     seasons_amount = valid_seasons_amount(args.seasons_amount)
     data = get_data(args.input_lightcurve)
@@ -85,8 +118,7 @@ spline_coordinates = x_spline, y_spline
 if args.display:
     display_plot(time, magnitude, spline_coordinates, centers)
 if args.image:
-    save_plot(time, magnitude, spline_coordinates,
-              centers, args.output_lightcurve)
+    save_plot(time, magnitude, spline_coordinates, centers, args.output_lightcurve)
 
 detrended_data = detrend_data(org_data, spline, magnitude.mean())
-savetxt(args.output_lightcurve, detrended_data, fmt="%16.6f %9.4f %7.4f")
+savetxt(args.output_lightcurve, detrended_data, fmt="%18.7f %15.7f %15.7f")
