d33cc2006b7db2787463e6f00df0889f3de8b1ff
Przemysław Bruś <19304263+pbrus@users.noreply.github.com>
2020-10-13 21:35:34 +0200

    Refactor fit.py script

--- varana/scripts/fit.py
+++ varana/scripts/fit.py
@@ -1,65 +1,80 @@
 #!/usr/bin/env python3
+"""
+Fit a sum of sines to the light curve.
 
+"""
 from argparse import ArgumentParser, RawTextHelpFormatter
 from textwrap import dedent
-from varana.fit import *
 
+from numpy import genfromtxt
 
-argparser = ArgumentParser(
-    prog='fit.py',
-    description='>> Fit a sum of sines to the light curve <<',
-    epilog='Copyright (c) 2019 Przemysław Bruś',
-    formatter_class=RawTextHelpFormatter
+from varana.fit import fit_final_curve, print_parameters, save_residuals
+
+arg_parser = ArgumentParser(
+    prog="fit.py",
+    description=">> Fit a sum of sines to the light curve <<",
+    epilog="Copyright (c) 2020 Przemysław Bruś",
+    formatter_class=RawTextHelpFormatter,
 )
 
-argparser.add_argument(
-    'lightcurve',
-    help=dedent('''\
+arg_parser.add_argument(
+    "lightcurve",
+    help=dedent(
+        """\
     The name of a file which stores light curve data.
     ------------------------------------
     The file must contain three columns:
     time magnitude magnitude_error
 
-    ''')
+    """
+    ),
 )
 
-argparser.add_argument(
-    '--freq',
-    help=dedent('''\
+arg_parser.add_argument(
+    "--freq",
+    help=dedent(
+        """\
     A list of frequencies for each sine.
 
-    '''),
-    nargs='+',
-    metavar='f1',
+    """
+    ),
+    nargs="+",
+    metavar="f1",
     type=float,
-    required=True
+    required=True,
 )
 
-argparser.add_argument(
-    '--resid',
-    help=dedent('''\
+arg_parser.add_argument(
+    "--resid",
+    help=dedent(
+        """\
     A name of the file storing residuals.
 
-    '''),
-    metavar='filename',
-    type=str
+    """
+    ),
+    metavar="filename",
+    type=str,
 )
 
-argparser.add_argument(
-    '--eps',
-    help=dedent('''\
+arg_parser.add_argument(
+    "--eps",
+    help=dedent(
+        """\
     Accuracy of comparison of frequencies.
-    (default = 0.001)
+    (default = 0.00001)
 
-    '''),
-    metavar='eps',
+    """
+    ),
+    metavar="eps",
     type=float,
-    default=1e-3
+    default=1e-5,
 )
 
-args = argparser.parse_args()
+args = arg_parser.parse_args()
+lightcurve = None
+
 try:
-    lightcurve = np.genfromtxt(args.lightcurve)
+    lightcurve = genfromtxt(args.lightcurve)
 except OSError as error:
     print(error)
     exit()
