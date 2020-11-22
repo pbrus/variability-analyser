be0e6117f54de4c92317cf0bddbf1edfede1c926
Przemysław Bruś <19304263+pbrus@users.noreply.github.com>
2020-11-22 22:35:18 +0100

    Restart a detrended file when changing the number of nodes interactively

--- lib/prepare_lc_lib.bash
+++ lib/prepare_lc_lib.bash
@@ -8,9 +8,10 @@ function rearrange_columns_lightcurve()
 
 function display_detrend_data()
 {
-    local output_file=${1/${LC_DIR}/${LC_DETREND_DIR}}
+    local output_file=$1.out
 
-    detrend.py $1 ${output_file%".tmp"} $2 --display
+    detrend.py $1 ${output_file} $2 --display
+    remove_files_or_dirs ${output_file}
 }
 
 function save_detrend_data()
@@ -22,24 +23,28 @@ function save_detrend_data()
 
 function detrend_data()
 {
+    local temporary_data_file=$1.tmp
     local nodes_number=$2
     local question="Is OK [Y/n]:"
     local answer="n"
 
+    cp $1 ${temporary_data_file}
     echo ${question}
-    display_detrend_data $1 ${nodes_number}
+    display_detrend_data ${temporary_data_file} ${nodes_number}
     read answer
 
     while [ "$answer" == "n" ]
     do
+        cp $1 ${temporary_data_file}
         echo "Number of nodes (last ${nodes_number}):"
         read nodes_number
         echo ${question}
-        display_detrend_data $1 ${nodes_number}
+        display_detrend_data ${temporary_data_file} ${nodes_number}
         read answer
     done
 
     save_detrend_data $1 ${nodes_number}
+    remove_files_or_dirs ${temporary_data_file}
     LC_NODES_NUMBER=${nodes_number}
 }
 
@@ -48,8 +53,7 @@ function detrend()
     if [ ! -e ${1/${LC_DIR}/${LC_DETREND_DIR}} ]
     then
         rearrange_columns_lightcurve $1
-        detrend_data $1.tmp ${LC_NODES_NUMBER}
-        remove_files_or_dirs $1.tmp
+        detrend_data $1 ${LC_NODES_NUMBER}
     fi
 }
 
